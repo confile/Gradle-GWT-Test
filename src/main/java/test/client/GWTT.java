60dd1051642aff4135633941dab85e51361f6012
Michael Gorski <info@michaelgorski.de>
2015-02-06 22:30:16 +0100

    test

---
+++ src/main/java/test/client/GWTT.java
@@ -0,0 +1,20 @@
+package test.client;
+
+import test.client.test2.MyWidgetGinjector;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+
+public class GWTT implements EntryPoint {
+
+
+	public void onModuleLoad() {
+
+//	 MyWidgetGinjector injector = Dagger_MyWidgetGinjector.create();
+//		 SomeService service = injector.getSomeService();
+//		
+//		 service.test();
+
+	}
+}
